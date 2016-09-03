fa558293964ec644e7c2f98081433731427dce46
Bia Beniamin <Bia Beniamin>
2016-09-03 17:12:42 +0300

    Documentatie

--- DesktopServer/DesktopServerLogical/Models/RemoteAction.cs
+++ DesktopServer/DesktopServerLogical/Models/RemoteAction.cs
@@ -13,6 +13,32 @@ namespace DesktopServerLogical.Models
         private ActionTypes _type;
         private Pin _pin;
         private int _value=0;
+        private DelegateCommand _removeCommand;
+        private Action<Pin, RemoteAction> _removeAction;
+        private Pin _ownerPin;
+
+        public Pin OwnerPin
+        {
+            get { return _ownerPin; }
+            set { _ownerPin = value; }
+        }
+
+        public Action<Pin, RemoteAction> RemoveAction
+        {
+            get
+            {
+                return _removeAction;
+            }
+            set
+            {
+                _removeAction = value;
+            }
+        }
+        public DelegateCommand RemoveCommand
+        {
+            get { return _removeCommand; }
+            set { _removeCommand = value; }
+        }
 
         public int Value
         {
@@ -34,10 +60,16 @@ namespace DesktopServerLogical.Models
                 _type = value;
             }
         }
-        public RemoteAction(Pin pin,ActionTypes type)
+        public RemoteAction(Pin pin,ActionTypes type,Pin ownerPin)
         {
             _pin = pin;
             _type = type;
+            _ownerPin = ownerPin;
+            _removeCommand = new DelegateCommand(Delete);
+        }
+        private void Delete()
+        {
+            _removeAction(_ownerPin, this);
         }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged(string name)
