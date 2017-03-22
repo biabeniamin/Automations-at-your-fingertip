414d183773a43594c0623223b0e61b0f0ac7c68a
biabeniamin <biabeniamin@gmail.com>
2017-03-22 17:01:20 +0200

    proto relay module

---
+++ RelayModuleProto/RelayModuleProto.ino
@@ -0,0 +1,42 @@
+//relay module
+#define ADDRESS 3
+#define TRIGGERED_PIN 9
+#define DEVICE_TYPE 1
+#include<SoftwareSerial.h>
+#include <Lan.h>
+SoftwareSerial serial(10, 11);
+int inputPinsCount = 0;
+int inputPins[1] = {8};
+int outputPinsCount = 1;
+int outputPins[1] = {9};
+int analogPinsCount = 0;
+int analogPins[1] = {0};
+int analogTriggeredValue[] = {4};
+void writeLan(int byte)
+{
+  serial.write(byte);
+}
+int readLan()
+{
+  return serial.read();
+}
+int countLan()
+{
+  return serial.available();
+}
+Lan lan(ADDRESS,DEVICE_TYPE,TRIGGERED_PIN, &writeLan, &readLan, &countLan);
+void setup()
+{
+  pinMode(8,INPUT_PULLUP);
+  serial.begin(9600);
+  Serial.begin(9600);
+  delay(1000);
+  lan.SetPins(&inputPinsCount,inputPins,&outputPinsCount,outputPins,&analogPinsCount,analogPins,analogTriggeredValue);
+  lan.Register();
+  
+}
+void loop() {
+  lan.CheckMessages();
+  lan.CheckAnalogPins();
+  lan.CheckInputPins();
+}
