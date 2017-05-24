c7739df5d9b80dd9ed03155efbac3d5f5a2dcfe8
biabeniamin <biabeniamin@gmail.com>
2017-05-24 11:39:13 +0300

    database in sql

--- DesktopServer/DesktopServerLogical/DatabaseOperations.cs
+++ DesktopServer/DesktopServerLogical/DatabaseOperations.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,19 @@ namespace DesktopServerLogical
 {
     public class DatabaseOperations
     {
-        private OleDbConnection _connection;
-        private OleDbCommand _command;
-        private OleDbDataReader _reader;
-        private OleDbDataAdapter _adapter;
+        private SqlConnection _connection;
+        private SqlCommand _command;
+        private SqlDataReader _reader;
+        private SqlDataAdapter _adapter;
         private DataSet _set;
         public DatabaseOperations()
         {
-            _connection = new OleDbConnection("Provider=Microsoft.Ace.Oledb.12.0;Data Source=db.accdb");
+            _connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Workspace\Grepit\DesktopServer\DesktopServerLogical\configurations.mdf;Integrated Security=True;Connect Timeout=30");
             _connection.Open();
         }
-        public OleDbDataReader GetReader(string query)
+        public SqlDataReader GetReader(string query)
         {
-            _command = new OleDbCommand(query, _connection);
+            _command = new SqlCommand(query, _connection);
             _reader = _command.ExecuteReader();
             return _reader;
         }
@@ -39,13 +40,13 @@ namespace DesktopServerLogical
         }
         public void ExecuteQuery(string query)
         {
-            _command = new OleDbCommand(query, _connection);
+            _command = new SqlCommand(query, _connection);
             _command.ExecuteNonQuery();
         }
-        public OleDbDataAdapter GetAdapter(string query)
+        public SqlDataAdapter GetAdapter(string query)
         {
-            _command = new OleDbCommand(query, _connection);
-            _adapter = new OleDbDataAdapter(_command);
+            _command = new SqlCommand(query, _connection);
+            _adapter = new SqlDataAdapter(_command);
             return _adapter;
         }
     }
--- DesktopServer/DesktopServerLogical/Saves.cs
+++ DesktopServer/DesktopServerLogical/Saves.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,7 @@ namespace DesktopServerLogical
         {
             foreach (RemoteAction action in actions)
             {
-                _dbOper.ExecuteQuery($@"insert into actions(pinId,Type,AValue,DeviceNumber,PinNumber) 
+                _dbOper.ExecuteQuery($@"insert into actions(pinId,Type,Value,DeviceNumber,PinNumber) 
                     values({pinId},{(int)action.Type},{action.Value},{action.Pin.Owner.Address},{action.Pin.PinNumber})");
                 //int actionid = _dbOper.ReadOneValue<int>($"select top 1 PinId from pins where Deviceid={deviceid} and PinNumber={pin.PinNumber} order by deviceid desc");
             }
@@ -39,35 +40,44 @@ namespace DesktopServerLogical
         {
             foreach (Device device in devices)
             {
-                _dbOper.ExecuteQuery($"insert into device(saveId,DeviceNumber) values({saveid},{device.Address})");
-                int deviceId = _dbOper.ReadOneValue<int>($"select top 1 DeviceId from device where DeviceNumber={device.Address} and saveId={saveid} order by deviceid desc");
+                _dbOper.ExecuteQuery($"insert into devices(saveId,DeviceNumber) values({saveid},{device.Address})");
+                int deviceId = _dbOper.ReadOneValue<int>($"select top 1 DeviceId from devices where DeviceNumber={device.Address} and saveId={saveid} order by deviceid desc");
                 AddPins(device.Pins, deviceId);
             }
         }
         public void AddSave(ObservableCollection<Device> devices, string name)
         {
-            _dbOper.ExecuteQuery($"insert into saves(nume) values('{name}')");
-            int saveId = _dbOper.ReadOneValue<int>($"select top 1 id from saves where nume='{name}' order by id desc");
+            _dbOper.ExecuteQuery($"insert into saves(name) values('{name}')");
+            int saveId = _dbOper.ReadOneValue<int>($"select top 1 id from saves where name='{name}' order by id desc");
             AddDevices(devices, saveId);
             /*for (int i = 0; i < actions.Count; i++)
             {
                 _dbOper.ExecuteQuery($"insert into actions(saveId,deviceId,pinId,Type,AValue) values({saveId},{actions[i].Pin.Owner.Address},{actions[i].Pin.PinNumber},{(int)actions[i].Type},{actions[i].Value})");
             }*/
         }
-        public ObservableCollection<RemoteAction> LoadActions(string name,ObservableCollection<Device> devices)
+        public ObservableCollection<Device> LoadActions(string name,ObservableCollection<Device> devices)
         {
             ObservableCollection<RemoteAction> actions = new ObservableCollection<RemoteAction>();
-            OleDbDataReader reader=_dbOper.GetReader($"select deviceId,pinId,Type,AValue from actions where saveId={_dbOper.ReadOneValue<int>($"select id from saves where nume='{name}'")}");
+            SqlDataReader reader=_dbOper.GetReader($@"SELECT TOP 1 device.DeviceNumber,Pins.PinNumber, Pins.TriggeredValue, Actions.DeviceNumber, Actions.PinNumber, Actions.Type, Actions.AValue
+FROM Saves, (device INNER JOIN Pins ON device.DeviceId = Pins.Deviceid) INNER JOIN Actions ON Pins.PinId = Actions.PinId
+WHERE(((Saves.Nume) = '{name}'))
+ORDER BY device.DeviceNumber DESC; ");
             while(reader.Read())
             {
-                int deviceId = Convert.ToInt32(reader[0]);
-                int pinId = Convert.ToInt32(reader[1]);
+                int ownerDeviceId = Convert.ToInt32(reader[0]);
+                int ownerPinId = Convert.ToInt32(reader[1]);
+                int triggeredValue= Convert.ToInt32(reader[2]);
+                int deviceId = Convert.ToInt32(reader[3]);
+                int pinId = Convert.ToInt32(reader[4]);
+                string type= reader[5].ToString();
+                int value= Convert.ToInt32(reader[6]);
                 Pin pin = Helpers.GetPin(Helpers.GetDevice(devices, deviceId), pinId);
-                RemoteAction action = new RemoteAction(pin,(ActionTypes)Enum.Parse(typeof(ActionTypes),reader[2].ToString()),pin);
-                action.Value = Convert.ToInt32(reader[3]);
-                actions.Add(action);
+                Pin ownerPin = Helpers.GetPin(Helpers.GetDevice(devices, ownerDeviceId), ownerPinId);
+                RemoteAction action = new RemoteAction(pin,(ActionTypes)Enum.Parse(typeof(ActionTypes),type),pin);
+                action.Value = value;
+                ownerPin.Actions.Add(action);
             }
-            return actions;
+            return devices;
         }
     }
 }
