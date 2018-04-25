d42b3b947c9938da2c5d2ee410c44371259bbfd8
biabeniamin <biabeniamin@gmail.com>
2018-04-25 21:02:16 +0300

    detects familiar faces

---
+++ DesktopServer/FacialRecognition/Face.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacialRecognition
+{
+    public class Face
+    {
+        private int _left;
+        private int _top;
+        private int _width;
+        private int _height;
+
+        public System.Windows.Int32Rect UIRect
+        {
+            get
+            {
+                return new System.Windows.Int32Rect(Left, Top, Width, Height);
+            }
+        }
+
+        public Face(int x, int y, int width, int height)
+        {
+            this._left = x;
+            this._top = y;
+            this._width = width;
+            this._height = height;
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public int Top
+        {
+            get { return _top; }
+            set { _top = value; }
+        }
+
+        public int Left
+        {
+            get { return _left; }
+            set { _left = value; }
+        }
+
+    }
+
+}
---
+++ DesktopServer/FacialRecognition/ImageHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace FacialRecognition
+{
+    public static class ImageHelpers
+    {
+        public static Tuple<int, int> GetImageSize(string imagePath)
+        {
+            var im = new BitmapImage();
+            im.BeginInit();
+            im.UriSource = new Uri(imagePath, UriKind.RelativeOrAbsolute);
+            im.EndInit();
+            return new Tuple<int, int>((int)im.Width, (int)im.Height);
+        }
+    }
+}
