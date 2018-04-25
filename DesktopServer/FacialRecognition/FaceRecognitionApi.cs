bcf7b52dc16379431c0599ded6a093811113db34
bb <biabeniamin@outlook.com>
2018-04-25 21:02:16 +0300

    detects familiar faces

---
+++ DesktopServer/FacialRecognition/FaceRecognitionApi.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacialRecognition
+{
+    public class FacialRecognitionApi
+    {
+        private FaceServiceClient _faceServiceClient = new FaceServiceClient(Subscription.SubscriptionKey, Subscription.Endpoint);
+        private LargePersonGroup _personGroup;
+        private async void GetLargePersonGroup()
+        {
+            var data = await _faceServiceClient.ListLargePersonGroupsAsync();
+            if (0 < data.Length)
+            {
+                _personGroup = data[0];
+            }
+        }
+
+        public async Task<List<Face>> DoesExists(String imagePath)
+        {
+            List<Face> facesLocation = new List<Face>();
+            // Call detection REST API
+            using (var fStream = File.OpenRead(imagePath))
+            {
+                try
+                {
+                    var faces = await _faceServiceClient.DetectAsync(fStream);
+                    foreach (var face in faces)
+                    {
+                        facesLocation.Add(new Face(face.FaceRectangle.Left,
+                            face.FaceRectangle.Top,
+                            face.FaceRectangle.Width,
+                            face.FaceRectangle.Height));
+                    }
+
+                    // Convert detection result into UI binding object for rendering
+                    var identifyResult = await _faceServiceClient.IdentifyAsync(faces.Select(ff => ff.FaceId).ToArray(), largePersonGroupId: _personGroup.LargePersonGroupId);
+
+                    bool wasRecognized = false;
+                    foreach (var result in identifyResult)
+                    {
+                        if (0 < result.Candidates.Length)
+                        {
+                            wasRecognized = true;
+                            break;
+                        }
+                    }
+
+                    if (true == wasRecognized)
+                    {
+                        System.Windows.MessageBox.Show("Was recognized");
+                    }
+                }
+                catch (FaceAPIException ex)
+                {
+                }
+            }
+            return facesLocation;
+        }
+
+        public FacialRecognitionApi()
+        {
+            GetLargePersonGroup();
+        }
+
+
+    }
+}
---
+++ DesktopServer/FacialRecognition/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FacialRecognition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Face> _faces;
+
+
+
+        public ObservableCollection<Face> Faces
+        {
+            get { return _faces; }
+            set
+            {
+                _faces = value;
+                OnPropertyChanged("Faces");
+
+            }
+        }
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            image.Source = new BitmapImage(new Uri(@"C:\Users\biabe\Pictures\Camera Roll\WIN_20180425_16_24_50_Pro.jpg"));
+            Faces = new ObservableCollection<Face>();
+            Faces.Add(new Face(0, 0, 50, 50));
+        }
+
+        private async void RunFacialRecognition()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "Image files(*.jpg, *.png, *.bmp, *.gif) | *.jpg; *.png; *.bmp; *.gif";
+            var result = dlg.ShowDialog();
+
+            if (result.HasValue && result.Value)
+            {
+                var facial = new FacialRecognitionApi();
+                List<Face> faces = await facial.DoesExists(dlg.FileName);
+                foreach (Face face in faces)
+                {
+                    int left = face.Left;
+                    int top = face.Top;
+                    int width = face.Width;
+                    int height = face.Height;
+                    Faces.Add(new Face(left, top, width, height));
+                }
+                image.Source = new BitmapImage(new Uri(dlg.FileName));
+            }
+            GC.Collect();
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            RunFacialRecognition();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string name)
+        {
+            if (null != PropertyChanged)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
