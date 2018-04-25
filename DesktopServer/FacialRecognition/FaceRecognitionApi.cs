6dfc127ca1ead4443d15e9024c55729e7908bd47
biabeniamin <biabeniamin@gmail.com>
2018-04-26 00:43:33 +0300

    facial recognition draws rectangle over face

--- DesktopServer/FacialRecognition/FaceRecognitionApi.cs
+++ DesktopServer/FacialRecognition/FaceRecognitionApi.cs
@@ -20,6 +20,8 @@ namespace FacialRecognition
             {
                 _personGroup = data[0];
             }
+
+            _personGroup.LargePersonGroupId = "1d321ce4-f343-4b92-8d68-1c97bd0ba007";
         }
 
         public async Task<List<Face>> DoesExists(String imagePath)
@@ -64,7 +66,45 @@ namespace FacialRecognition
             return facesLocation;
         }
 
-        public FacialRecognitionApi()
+        public async void AddFace(String imagePath, String name)
+        {
+            using (var fStream = File.OpenRead(imagePath))
+            {
+                try
+                {
+                    var faces = await _faceServiceClient.DetectAsync(fStream);
+
+
+                    var identifyResult = await _faceServiceClient.IdentifyAsync(faces.Select(ff => ff.FaceId).ToArray(), largePersonGroupId: _personGroup.LargePersonGroupId);
+
+                    bool wasRecognized = false;
+                    foreach (var result in identifyResult)
+                    {
+                        if (0 == result.Candidates.Length)
+                        {
+                            Person person = new Person();
+                            person.Name = name;
+                            person.PersonId = (await _faceServiceClient.CreatePersonInLargePersonGroupAsync(_personGroup.LargePersonGroupId,
+                                name)).PersonId;
+
+                            using (var fStream2 = File.OpenRead(imagePath))
+                            {
+                                var res = await _faceServiceClient.AddPersonFaceInLargePersonGroupAsync(_personGroup.LargePersonGroupId,
+                                person.PersonId,
+                                fStream2,
+                                imagePath);
+                            }
+                        }
+                    }
+
+                }
+                catch (FaceAPIException ex)
+                {
+                }
+            }
+        }
+
+                public FacialRecognitionApi()
         {
             GetLargePersonGroup();
         }
